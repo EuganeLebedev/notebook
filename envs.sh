8f316e97872c1606cfd4539997e4c74a392facbb
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-10 17:54:40 +0300

    Change DB to psql; add docker-compose with pgadmin and postgres containers; Hide settings inte system variables

---
+++ envs.sh
@@ -0,0 +1,9 @@
+export POSTGRES_DB=notebook
+export POSTGRES_USER=notebook_user
+export POSTGRES_PASSWORD=123qweASD
+
+export PGADMIN_DEFAULT_EMAIL=info@local.home
+export PGADMIN_DEFAULT_PASSWORD=123qweASD
+export PGADMIN_CONFIG_SERVER_MODE=False
+
+export DJANGO_SECRET_KEY=dfzaljknhwESDFJL1243ERHN
--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-31r7tcbc=p_5ucr9!9)_@f4!as4=s(^&*it06zpr-2!m@yv*p^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'notebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'notebook',
+        'USER' : 'notebook_user',
+        'PASSWORD' : '123qweASD',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
