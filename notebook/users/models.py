6ff708dfe298129c85168db7d1cae83117764b6b
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-10 21:02:31 +0300

    Add author model

--- notebook/users/models.py
+++ notebook/users/models.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Author(models.Model):
+    first_name = models.CharField(blank=False, max_length=255)
+    last_name = models.CharField(blank=False, max_length=255)
+    username = models.OneToOneField(User, on_delete=models.PROTECT)
