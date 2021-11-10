eb03d067070de94c246f81b8d6a44b27a3d551fc
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-10 21:21:33 +0300

    Add created_at and updated_add fields into Author model

--- notebook/users/models.py
+++ notebook/users/models.py
@@ -9,3 +9,5 @@ class Author(models.Model):
     first_name = models.CharField(blank=False, max_length=255)
     last_name = models.CharField(blank=False, max_length=255)
     username = models.OneToOneField(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
