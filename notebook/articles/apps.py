8eb5975cd2a8b4fef238f4ed94ebe3ba9931c9da
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-18 22:00:12 +0300

    Add app_name

--- notebook/articles/apps.py
+++ notebook/articles/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+app_name = 'articles'
+
 
 class ArticlesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- notebook/users/models.py
+++ notebook/users/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class User(AbstractUser):
     pass
+
 
 class Author(models.Model):
     first_name = models.CharField(blank=False, max_length=255)
