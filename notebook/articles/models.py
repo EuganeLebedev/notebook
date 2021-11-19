95db5b45245291c89ffadd8c2fef6a1fadaffd93
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-19 13:04:01 +0300

    Add slug field into post

--- notebook/articles/models.py
+++ notebook/articles/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    slug = models.SlugField(max_length=255, unique=True, null=False,
+                            db_index=True, verbose_name='Title')
     post_content = models.TextField(blank=False)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(self, *args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'pk': self.slug})
 
 
 class Category(models.Model):
