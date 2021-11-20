2913fd6ca67b21ef9f68956219cf2633c4afc7d2
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-20 12:05:27 +0300

    Add PostCreate View into articles app

--- notebook/articles/views.py
+++ notebook/articles/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
 
+from articles.models import Post
 # Create your views here.
+
+
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'articles/post_create.html'
+    fields = ['title', 'post_content', 'categoryes']
+
+
+class PostShow(DetailView):
+    pass
+
+
+class PostDelete(DeleteView):
+    pass
+
+
+class PostList(ListView):
+    pass
