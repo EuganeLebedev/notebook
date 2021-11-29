be3a4aa99c561de8ae4b166b283250c4a95456f3
EuganeLebedev <adm1n.lebedev@ya.ru>
2021-11-29 20:19:54 +0300

    Вывод дипломов по группам

--- notebook/index/views.py
+++ notebook/index/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from index.models import Regards
+from index.models import Regards, Category
 
 # Create your views here.
 
@@ -10,5 +10,5 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["regards_images"] = Regards.objects.all()
+        context["categoryes"] = Category.objects.all()
         return context
